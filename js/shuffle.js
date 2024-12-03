c8cd93346c84e21346e6778ec9d18f6427c0067e
timber <crylingio@gmail.com>
2024-12-02 22:48:04 -0700

    Add files via upload

---
+++ js/shuffle.js
@@ -0,0 +1,39 @@
+const imageSources = [
+  {
+    src: "./img/ads/ad1.png",
+    alt: "freestuff", 
+    href: "https://www.archive.org"
+  },
+  {
+    src: "./img/ads/kleiner.jpg",
+    alt: "kleiner",
+    href: "https://www.youtube.com/watch?v=M9J6DKJXoKk"
+  },
+  {
+    src: "./img/ads/music.gif",
+    alt: "music",
+    href: "./music.html"
+  }
+];
+
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
+const imgs = document.querySelectorAll('.shuffle');
+
+function win(sources, imageList) {
+  shuffle(sources);
+  for (let i = 0; i <= imageList.length - 1; i++){
+    imageList[i].setAttribute("src", sources[i].src);
+    imageList[i].setAttribute("alt", sources[i].alt);
+    imageList[i].parentElement.setAttribute("href", sources[i].href);
+  }
+};
+
+window.addEventListener('load', () => {
+    win(imageSources,imgs);
+});
